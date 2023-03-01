d1cb728826c8f64febe3e667bd2d40716d807efd
meselmiasMarques <49796344+meselmiasMarques@users.noreply.github.com>
2023-02-28 21:13:57 -0300

    Commit

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,140 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("v1/categories")]
+        public async Task<IActionResult> GetAsync(
+            [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                var categories = await context.Categories.ToListAsync();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05xe5 - Falha interna no Servidor");
+            }
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                var category = await context
+                    .Categories
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound();
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "05xe10 - Falha interna no Servidor");
+            }
+
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> PostAsync(
+           [FromBody] EditorCategoryViewModel model,
+           [FromServices] BlogDataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            try
+            {
+                var category = new Category { 
+                Id = 0,
+                Posts = null,
+                Name = model.Name,
+                Slug = model.Slug.ToLower(),
+                };
+                await context.Categories.AddAsync(category);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{category.Id}", category);
+            }
+            catch(DbUpdateException ex)
+            {
+                return StatusCode(500,"05xe9 - Não foi possível Incluir a Categoria");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "05xe11 - Falha interna no Servidor");
+            }
+
+           
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<IActionResult> PutAsync(
+            [FromRoute] int id,
+            [FromBody] EditorCategoryViewModel model,
+            [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                var category = await context
+                        .Categories
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound();
+
+                category.Name = model.Name;
+                category.Slug = model.Slug.ToLower();
+
+                context.Categories.Update(category);
+                await context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "05xe15 - Falha interna no Servidor");
+            }
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<IActionResult> DeleteAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            try
+            {
+                var category = await context
+                    .Categories
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == id);
+                if (category == null)
+                    return NotFound();
+
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "05xe15 - Falha interna no Servidor");
+            }
+        }
+    }
+}
---
+++ Blog/ViewModels/EditorCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
+{
+    public class EditorCategoryViewModel
+    {
+        [Required(ErrorMessage ="O Campo {0} é requerido")]
+        [StringLength(40, MinimumLength =3, ErrorMessage = "Este campo deve conter entre 3 e 40 Caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é requerido")]
+        public string Slug { get; set; }
+    }
+}
